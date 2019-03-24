a49bbcbfcaff7be35a7702e0df0055c78d4f5a79
Christopher Robert Philabaum <christopher@philabaum.me>
2019-03-24 00:49:24 -0700

    Added support parsing hex strings.

--- src/util.c
+++ src/util.c
@@ -184,3 +184,47 @@ void fprint_hex(FILE *stream, const unsigned char *array, size_t count) {
         fprintf(stream, "%02x", array[i]);
     }
 }
+
+/// Parse an individual hexadecimal character to an integer 0 to 15.
+/// \param hex_char An individual hexadecimal character.
+/// \return Return 0 to 15 depending on the value of hex_char, else return -1 on an invalid character.
+int parse_hex_char(char hex_char) {
+    if(hex_char >= '0' && hex_char <= '9') {
+        return hex_char - 48;
+    }
+    else if(hex_char >= 'A' && hex_char <= 'F') {
+        return hex_char - 55;
+    }
+    else if(hex_char >= 'a' && hex_char <= 'f') {
+        return hex_char - 87;
+    }
+    else {
+        return -1;
+    }
+}
+
+int parse_hex(unsigned char *array, char *hex_string) {
+    size_t i, b;
+    int value;
+
+    for(i = 0, b = 0; hex_string[b] != '\0'; i++) {
+        if((value = parse_hex_char(hex_string[b++])) < 0) {
+            return 1;
+        }
+
+        array[i] = (unsigned char)value << 4;
+
+        // The length of hex string was odd
+        if(hex_string[b] == '\0') {
+            return 2;
+        }
+
+        if((value = parse_hex_char(hex_string[b++])) < 0) {
+            return 1;
+        }
+
+        array[i] |= (unsigned char)value;
+    }
+
+    return 0;
+}
--- src/util.h
+++ src/util.h
@@ -74,5 +74,11 @@ void uint256_get_perm_pair(uint256_t *starting_perm, uint256_t *ending_perm, siz
 /// \param array An allocated byte array to print.
 /// \param count The # of bytes to print from array.
 void fprint_hex(FILE *stream, const unsigned char *array, size_t count);
+/// Unparse a hex string to a byte array. The hex string is assumed to be null-terminated.
+/// \param array An allocated byte array to parse to.
+/// \param hex_string A null-terminated hex string.
+/// \return Returns 0 on success, 1 if the string contains any invalid characters, or 2
+/// if the hex string length is odd.
+int parse_hex(unsigned char *array, char *hex_string);
 
 #endif //HAMMING_BENCHMARK_UTIL_H
