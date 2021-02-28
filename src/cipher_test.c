a39b4605a414fb6081116848467b4557c8a40d7c
Christopher Philabaum <christopher@philabaum.me>
2021-02-27 22:05:34 -0700

    Made the cipher size determined on runtime for cipher_test.

--- src/cipher_test.c
+++ src/cipher_test.c
@@ -21,7 +21,7 @@ int main() {
             0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
             0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
     };
-    // A message that's exactly 16 bytes long.
+    // A message that's exactly 16 bytes long (excluding the null character)
     const char msg[] = "Hello world x2!\n";
     const unsigned char expected_cipher[] = {
             0x00, 0x80, 0xb5, 0xcd, 0x7d, 0x63, 0x1b, 0x04,
@@ -31,7 +31,10 @@ int main() {
     unsigned char cipher[16];
     int status;
 
-    if(evp_encrypt_msg(cipher, NULL, EVP_aes_256_ecb(), key, msg, 16, NULL)) {
+    const EVP_CIPHER *evp_cipher = EVP_aes_256_ecb();
+
+    if(evp_encrypt_msg(cipher, NULL, evp_cipher, key, (const unsigned char*)msg,
+                       EVP_CIPHER_block_size(evp_cipher), NULL)) {
         fprintf(stderr, "ERROR: evp_encrypt_msg failed\n");
         return EXIT_FAILURE;
     }
