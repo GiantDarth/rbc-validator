7debb0f832ff01d0ddd14d2004d5962ff9c7fa69
Christopher Robert Philabaum <christopher@philabaum.me>
2021-05-14 22:22:35 -0700

    Re-added missing break for OpenMP loop to exit early (#116)

--- src/validator.c
+++ src/validator.c
@@ -282,6 +282,7 @@ int find_matching_seed(unsigned char *client_seed, const unsigned char *host_see
             // keys
 #pragma omp critical
             memcpy(client_seed, curr_seed, SEED_SIZE);
+            break;
 #endif
         }
 
